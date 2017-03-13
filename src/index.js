969dc7b611b0a59269a4b12545ae768fe6b4cb2f
turbobeast <georgemichaelfaust@gmail.com>
2017-03-13 15:47:20 -0400

    step 3: moved store into it's own module

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App/App";
 import "./index.css";
-
-import appReducer from './reducers'
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createLogger from 'redux-logger';
-import reduxThunk from 'redux-thunk';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-const logger = createLogger();
-const store = createStore(appReducer, applyMiddleware(logger, reduxThunk));
-
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,5 +12,5 @@ ReactDOM.render(
       <Route component={App} />
     </Router>
   </Provider>, 
-    document.getElementById("root")
+  document.getElementById("root")
 );
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import appReducer from './reducers'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
+
+const logger = createLogger();
+export default createStore(appReducer, applyMiddleware(logger, reduxThunk));
