1b191075f6acc0207c7a86eaedfc8b117c931e96
turbobeast <georgemichaelfaust@gmail.com>
2017-03-13 12:13:29 -0400

    step 4: added router and routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import "./App.css";
 import RobotFilterViewContainer from '../../Containers/RobotFilterViewContainer';
 import ProfileViewContainer from '../../Containers/ProfileViewContainer';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="tc">
       <h1>RoboDex</h1>
-        {/*<RobotFilterViewContainer />*/}
-        <ProfileViewContainer />
+        <Route path="/" exact={true} component={RobotFilterViewContainer} />
+        <Route path="/profile/:id" component={ProfileViewContainer} />
     </div>
   );
 }
