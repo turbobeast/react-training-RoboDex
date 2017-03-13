958b21e516d8535b48070e920535d19eb0b7da9d
turbobeast <georgemichaelfaust@gmail.com>
2017-03-13 16:00:03 -0400

    step 4: handling react and es2015 imports

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+require('babel-register')({
+  extensions: ['.js'],
+  presets: ['es2015', 'react-app'],
+})
+
+require('ignore-styles');
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8080;
 const { htmlTemplate } = require('./views/index.html');
 const manifest = require('./build/asset-manifest.json');
+
+const store = require('./src/store').default;
+const App = require('./src/Components/App/App').default;
 
 app.use('*/static', express.static(path.join(__dirname, 'build', 'static')));
 app.use((req, res) => {
