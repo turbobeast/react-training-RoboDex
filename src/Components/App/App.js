dc40a05bb7a8de14b9ced004621ac3b265359784
turbobeast <georgemichaelfaust@gmail.com>
2017-03-13 11:21:02 -0400

    step 2: moved App.js to components

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import RobotFilterViewContainer from '../../Containers/RobotFilterViewContainer';
+
+function App() {
+  return (
+    <div className="tc">
+      <h1>RoboDex</h1>
+        <RobotFilterViewContainer />
+    </div>
+  );
+}
+
+export default App;
