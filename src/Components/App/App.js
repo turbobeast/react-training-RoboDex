a6ef3c308b416d99d8cfd4f136c2b73dd3c62401
turbobeast <georgemichaelfaust@gmail.com>
2017-03-15 11:20:03 -0400

    step 7: 404

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import "./App.css";
 import RobotFilterViewContainer from '../../Containers/RobotFilterViewContainer';
 import ProfileViewContainer from '../../Containers/ProfileViewContainer';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="tc">
       <h1>RoboDex</h1>
-        <Route path="/" exact={true} component={RobotFilterViewContainer} />
-        <Route path="/profile/:id" component={ProfileViewContainer} />
+        <Switch>
+          <Route path="/" exact={true} component={RobotFilterViewContainer} />
+          <Route path="/profile/:id" component={ProfileViewContainer} />
+          <Route render={() => <h2>Page Not Found</h2> } />
+        </Switch>
     </div>
   );
 }
