6074cdb786f7da05a357e6665b77a371231488f6
turbobeast <georgemichaelfaust@gmail.com>
2017-03-15 14:44:05 -0400

    step 9: validating id param for profile route"

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,7 +10,14 @@ function App() {
       <h1>RoboDex</h1>
         <Switch>
           <Route path="/" exact={true} component={RobotFilterViewContainer} />
-          <Route path="/profile/:id" component={ProfileViewContainer} />
+          <Route path="/profile/:id" render={(props) => {
+            const rawId = props.match.params.id;
+            const intId = parseInt(rawId, 10);
+            if (!isNaN(intId) && intId <= 10 && intId > 0) {
+              return <ProfileViewContainer {...props} />
+            }
+            return <h2>No Robot for id {rawId}</h2>
+          }} />
           <Route render={() => <Redirect to={{ pathname: '/' }} />} />
         </Switch>
     </div>
